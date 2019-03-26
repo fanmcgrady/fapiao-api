c643a2b238c85fbff826f73a064075cced895e69
fangzhiyang <fanmcgrady@gmail.com>
2019-03-26 21:43:22 +0800

    update shell

--- restart_debug.sh
+++ restart_debug.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # 本shell用于debug模式启动django项目
 # 关闭原有服务
-ps -ef|grep "runserver 0.0.0.0:8001"|grep -v grep|cut -c 10-15|xargs kill -9
+ps -ef|grep "runserver 0.0.0.0:8000"|grep -v grep|cut -c 10-15|xargs kill -9
 # 切换到anaconda环境
-source activate fapiao-api-deploy
+#source activate fapiao-api
 # 更新代码
 #git pull
 # 前台启动django服务
-python manage.py runserver 0.0.0.0:8001 --insecure
+python manage.py runserver 0.0.0.0:8000 --insecure
