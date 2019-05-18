45ff2c77debf9d7cdfee72153697282a6567bbc3
fangzhiyang <fanmcgrady@gmail.com>
2019-05-18 12:16:33 +0800

    多线程

---
+++ pool.py
@@ -0,0 +1,19 @@
+from multiprocessing import Pool
+import os, time, random
+import API_test as api
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    # time.sleep(random.random() * 3)
+    print(api.run_type())
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+
+if __name__=='__main__':
+    p = Pool()
+    for i in range(50):
+        p.apply_async(long_time_task, args=(i,))
+    p.close()
+    p.join()
